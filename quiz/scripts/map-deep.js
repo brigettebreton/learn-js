889c92dbb8b58b8761df8573a99fe2e7dcc775c1
Gigi Breton <gigibreton@MacBook-Air.local>
2024-02-06 15:37:08 -0500

    Fixed to create a shallow copy

--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -6,7 +6,8 @@ const originalMatrix = [
   ];
   
   const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
+    return Array.from(row).sort();
+    // can also use return [...row].sort();
   });
   
   // Modifying the original matrix (changing the last element of the first row)
